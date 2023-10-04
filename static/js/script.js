307a64d02dfcfa868b7d416c77a7bc18941b6eb7
Abner Eger <abner.juda.eger.santana@gmail.com>
2023-10-04 16:53:03 -0300

    feat: :sparkles: adicionando introdução

---
+++ static/js/script.js
@@ -0,0 +1,13 @@
+document.addEventListener('DOMContentLoaded', function () {
+    const mostrarSidebarButton = document.getElementById('mostrarSidebar');
+    const fecharSidebarButton = document.getElementById('fecharSidebar');
+    const minhaSidebar = document.getElementById('minhaSidebar');
+
+    mostrarSidebarButton.addEventListener('click', function () {
+        minhaSidebar.style.left = '0'; // Mostra a barra lateral
+    });
+
+    fecharSidebarButton.addEventListener('click', function () {
+        minhaSidebar.style.left = '-290px'; // Esconde a barra lateral
+    });
+});
