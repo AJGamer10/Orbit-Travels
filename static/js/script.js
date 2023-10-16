63c7b17f9e435f652330c49445f7039d0af22ec6
AbnerEger <abner.juda.eger.santana@gmail.com>
2023-10-16 00:46:33 -0300

    feat: :sparkles: paginas avaliações e contatos

--- static/js/script.js
+++ static/js/script.js
@@ -28,3 +28,54 @@ document.addEventListener('DOMContentLoaded', function () {
         modalLogin.style.left = '100%'; // Esconde a barra lateral
     });
 })
+
+// Estrelas para avaliação
+Avaliar(estrela); {
+    const star1 = document.getElementById("star1").src;
+    const star2 = document.getElementById("star2").src;
+    const star3 = document.getElementById("star3").src;
+    const star4 = document.getElementById("star4").src;
+    const star5 = document.getElementById("star5").src;
+
+    var list = [star1, star2, star3, star4, star5]
+
+    if (estrela == 5) {
+        for (var i = 0; i < 6; i++) {
+            list[i].style.color = 'yellow'
+        }
+    }
+
+    if (estrela == 4) {
+        for (var i = 0; i < 5; i++) {
+            list[i].style.color = 'yellow'
+        }
+        star5.style.color = 'gray'
+    }
+
+    if (estrela == 3) {
+        for (var i = 0; i < 4; i++) {
+            list[i].style.color = 'yellow'
+        }
+        star4.style.color = 'gray'
+        star5.style.color = 'gray'
+    }
+
+    if (estrela == 2) {
+        for (var i = 0; i < 3; i++) {
+            list[i].style.color = 'yellow'
+        }
+        star3.style.color = 'gray'
+        star4.style.color = 'gray'
+        star5.style.color = 'gray'
+    }
+
+    if (estrela == 1) {
+        for (var i = 0; i < 2; i++) {
+            list[i].style.color = 'yellow'
+        }
+        star2.style.color = 'gray'
+        star3.style.color = 'gray'
+        star4.style.color = 'gray'
+        star5.style.color = 'gray'
+    }
+}
